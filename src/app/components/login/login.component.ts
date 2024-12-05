16a71ef3caaf5d718b179ab22f1d17a0b42b7ecb
Nhi Uyen Bui Nguyen <83212426+annauyen@users.noreply.github.com>
2024-12-05 12:50:51 +0700

    update image

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -10,39 +10,40 @@ import OktaSignIn from '@okta/okta-signin-widget';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
   oktaSignin: any;
   private oktaAuth = inject(OKTA_AUTH);
 
   constructor() {
-
     this.oktaSignin = new OktaSignIn({
-      logo: 'assets/images/logo.png',
+      logo: 'assets/images/icon.png',
       baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
       clientId: myAppConfig.oidc.clientId,
       redirectUri: myAppConfig.oidc.redirectUri,
       authParams: {
         pkce: true,
         issuer: myAppConfig.oidc.issuer,
-        scopes: myAppConfig.oidc.scopes
+        scopes: myAppConfig.oidc.scopes,
       },
       features: {
-        registration: true
+        registration: true,
       },
       helpLinks: {
         help: null,
         forgotPassword: null,
         unlock: null,
-        custom: []
-      }
+        custom: [],
+      },
     });
-   }
+  }
 
   ngOnInit(): void {
     this.oktaSignin.remove();
 
-    this.oktaSignin.renderEl({
-      el: '#okta-sign-in-widget'}, // this name should be same as div tag id in login.component.html
+    this.oktaSignin.renderEl(
+      {
+        el: '#okta-sign-in-widget',
+      }, // this name should be same as div tag id in login.component.html
       (response: any) => {
         if (response.status === 'SUCCESS') {
           this.oktaAuth.signInWithRedirect();
@@ -52,6 +53,5 @@ export class LoginComponent implements OnInit{
         throw error;
       }
     );
-
   }
 }
