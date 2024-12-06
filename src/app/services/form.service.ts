62f63b10ae6886fdeb5add24e18e0cc11f57dbc9
le-phuc-loc <lephucloc1711@gmail.com>
2024-12-06 22:19:40 +0700

    add proxy

--- src/app/services/form.service.ts
+++ src/app/services/form.service.ts
@@ -6,8 +6,8 @@ import { Observable, of } from 'rxjs';
   providedIn: 'root',
 })
 export class FormService {
-  private countriesUrl = 'https://103.188.82.184:8443/api/countries';
-  private statesUrl = 'https://103.188.82.184:8443/api/states';
+  private countriesUrl = '/api/countries';
+  private statesUrl = '/api/states';
 
   constructor(private httpClient: HttpClient) {}
 
